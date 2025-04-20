45f7f2a6c17084ea9ca28f599cdada1dd5175f79
DaytripperTX <chad98tx@gmail.com>
2025-04-19 23:50:57 -0500

    Reenable visibility patch

--- VisibilityFixMod/Patches/Patches.cs
+++ VisibilityFixMod/Patches/Patches.cs
@@ -6,9 +6,9 @@ using static MelonLoader.MelonLogger;
 namespace VisibilityFixMod.Patches
 {
     class Patches
-    {/*
-        [HarmonyPatch(typeof(ScheduleOne.Stealth.PlayerVisibility))] 
-        [HarmonyPatch("CalculateVisibility")]                  
+    {
+        [HarmonyPatch(typeof(ScheduleOne.Stealth.PlayerVisibility))]
+        [HarmonyPatch("CalculateVisibility")]
         class VisibilityPatch
         {
             static bool Prefix(ref float __result)
@@ -16,7 +16,7 @@ namespace VisibilityFixMod.Patches
                 __result = Config.MaxVisibility; // Minimum visibility
                 return false;  // Skip the original method
             }
-        }*/
+        }
 
         [HarmonyPatch(typeof(ScheduleOne.Stealth.PlayerVisibility), "CalculateVisibility")]
         public static class DebugVisibility
