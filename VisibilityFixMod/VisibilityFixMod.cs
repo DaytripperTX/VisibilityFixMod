5217b95eb33d308c2d29c395efcdffe14be5eb24
DaytripperTX <chad98tx@gmail.com>
2025-04-21 21:44:17 -0500

    Updated version to 1.0.0 for initial release

--- VisibilityFixMod/VisibilityFixMod.cs
+++ VisibilityFixMod/VisibilityFixMod.cs
@@ -1,6 +1,6 @@
 ﻿using MelonLoader;
 
-[assembly: MelonInfo(typeof(VisibilityFixMod.VisibilityFix), "VisibilityFix", "0.5.0", "volcomtx")]
+[assembly: MelonInfo(typeof(VisibilityFixMod.VisibilityFix), "VisibilityFix", "1.0.0", "volcomtx")]
 [assembly: MelonGame("TVGS", "Schedule I")]
 
 namespace VisibilityFixMod
