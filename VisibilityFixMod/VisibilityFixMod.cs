37fafbea7535cab216de3cf2b9ba9a352b3c40ac
DaytripperTX <chad98tx@gmail.com>
2025-04-23 01:04:02 -0500

    ported ModManagerIntegration from master branch

--- VisibilityFixMod/VisibilityFixMod.cs
+++ VisibilityFixMod/VisibilityFixMod.cs
@@ -1,21 +1,27 @@
 ﻿using MelonLoader;
 
-[assembly: MelonInfo(typeof(VisibilityFixMod.VisibilityFix), "VisibilityFix", "1.0.0", "volcomtx")]
+[assembly: MelonInfo(typeof(VisibilityFixMod.VisibilityFix), "VisibilityFix", "1.0.4", "volcomtx")]
 [assembly: MelonGame("TVGS", "Schedule I")]
 
 namespace VisibilityFixMod
 {
     public class VisibilityFix : MelonMod
     {
-        public override void OnApplicationStart()
+        public override void OnInitializeMelon()
         {
             Config.Load();
             var harmony = new HarmonyLib.Harmony("com.visibilityfix");
             harmony.PatchAll();
             LoggerInstance.Msg($"Visibility Fix loaded. MaxVisibility = {Config.MaxVisibility}");
 
-            if(Config.EnableDebugLogs)
+            if (Config.EnableDebugLogs)
                 LoggerInstance.Msg("Debug Logs Enabled");
         }
+
+        public override void OnDeinitializeMelon()
+        {
+            ModManagerIntegration.Deinitialize();
+        }
+
     }
 }
